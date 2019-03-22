87188db82975e2fa92688ad487c2eaddb3bec1a9
Diogo Borges Santos <diogo.borges@extremedigital.com.br>
2019-03-22 16:12:44 -0300

    update

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const fastify = require('fastify')();
 fastify.get('/', (req, res) => {
   res.send({ Hello: "World!" })
 })
-fastify.listen(3000).then(() => console.log(`server listening on ${fastify.server.address().port}`))
+fastify.listen(8080).then(() => console.log(`server listening on ${fastify.server.address().port}`))
 
 // Connect to the db
 mongoose.connect("mongodb://localhost:27017/openshiftDB", function (err) {
-  console.log('Connected');
+  if (err) console.log(err)
+
+  console.log('Connected by Diogo');
   const usersSchema = new mongoose.Schema({
     name: { type: String }
   });
 
   const User = mongoose.model('User', usersSchema);
 
-  User.create({ name: 'teste' }).then(() => console.log('user Created'))
+  User.create({ name: 'teste' }).then(() => console.log('user Created by Diogo'))
 });
 
 
